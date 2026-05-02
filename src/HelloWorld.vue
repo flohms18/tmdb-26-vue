61b99c0f70693a880f9c198089978501357fc99d
flohms18 <trouilletflorent@outlook.com>
2026-05-02 22:04:31 +0200

    Updated AXIOS

--- src/HelloWorld.vue
+++ src/HelloWorld.vue
@@ -1,4 +1,27 @@
-<script setup></script>
+<script setup>
+import axios from 'axios';
+import { ref, onMounted } from 'vue';
+
+import.meta.env.VUE_APP_MY_TMDBAPI_KEY
+
+const movies = ref([])
+
+const fetchMovies = async () => {
+    const response = await axios.get('https://api.themoviedb.org/3/movie/popular', {
+        headers: {
+            Authorization: `Bearer ${import.meta.env.VITE_MY_TMDBAPI_KEY
+
+        }
+    })
+}
+
+
+
+
+
+
+
+</script>
 
 <template>
   
